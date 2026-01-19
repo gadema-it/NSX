de7eda7347ddf3d45d598af1857c4558f5409f5b
Gabriele De Martin <gabriele@gadema.it>
2026-01-19 15:40:18 +0100

    fix compatibility profile issue in win11 and linux

--- src/main.cpp
+++ src/main.cpp
@@ -13,17 +13,14 @@ int main(int argc, char *argv[])
     //format.setSwapInterval(0); // TODO https://stackoverflow.com/questions/5829881/avoid-waiting-on-swapbuffers
     format.setDepthBufferSize(24);
     format.setStencilBufferSize(8);
-    format.setVersion(4, 2);
+    //format.setVersion(4, 2);
     format.setProfile(QSurfaceFormat::CompatibilityProfile);
-   // format.setProfile(QSurfaceFormat::CoreProfile);
-    QSurfaceFormat::setDefaultFormat(format);
 
+    QSurfaceFormat::setDefaultFormat(format);
 
     QCoreApplication::setAttribute(Qt::AA_ShareOpenGLContexts);
     Application &application = Application::instance();
     application.setStyle(QStyleFactory::create("windows"));
-
-
 
 //    QOpenGLContext openGLContext;
 //    openGLContext.setFormat(format);
@@ -39,15 +36,9 @@ int main(int argc, char *argv[])
 //    Material *m = new Material();
 
     MainWindow w;
-
     application.mainWindow = &w;
-
     application.init();
-
-    //this->mainWindow = w;
     w.show();
 
-
-    //qDebug() <<   QOpenGLContext::currentContext()->isValid();
     return application.exec();
 }
